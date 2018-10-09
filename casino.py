9c857a003707fb0df340c20880cf7ea96038cc0e
yeeter1 <44006895+yeeter1@users.noreply.github.com>
2018-10-09 17:48:57 -0400

    Update casino.py

--- casino.py
+++ casino.py
@@ -1,40 +1,31 @@
 import json
 import math
 import sys
-import random
-payday = True
-import json
 
-def writeToJSONFile(writing, filename):
-    with open(filename + '.json', 'w') as output:
-        json.dump(data,  output)
+def writeToJSONFile(data, filename):
+    f = open(filename + '.json', 'w')
+    json.dump(data, f)
+
 
 try:
     open("data.json")
 except:
-    input(".json data file not found! Aborting!")
-    sys.exit(1)
+    input("Failed to load data.json! Make sure its in the same directory as casino.py")
 else:
-    data = open("data.json", "r")
+    data = open("data.json")
+    data = json.load(data)
     listData = data
-    data = json.load(data)[0]
+    data = data[0]
     if not data['credits']:
         data['credits'] = 10
+        listData[0]['credits'] = 10
     credits = data['credits']
 finally:
-    print("Your current credit balance is: " + str(credits))
-    whattodo = input("What would you like to do? (slot: x, claim, shop) ")
-if whattodo.find("slot") != -1:
-    creditsslot = whattodo[5:]
+    print("Current credit balance: " + str(credits))
+    doing = input("What would you like to do? (slot: x, claim, slot) ")
+if doing.find("slot") != 1:
+    creditsslot = doing[5:]
     credits = credits - int(creditsslot)
-    writeToJSONFile({"credits": credits}, "data")
-    input("You now have " + str(credits) + " credits")
-if whattodo.find("claim") != -1:
-   if payday != True:
-       print("Come back later for your payday.")
-   else:
-       credits = credits + 150
-       payday = False
-       print("You now have " + str(credits) + " credits.")
-if whattodo.find("shop") != -1:
-    print("Shop is currently WIP")
+    listData[0]['credits'] = credits
+    print("You now have " + str(credits) + " credits.")
+    writeToJSONFile(listData, "data")
