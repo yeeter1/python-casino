4d92dd7165ad2f29cf076aa0e04d79a421e30afd
yeeter1 <44006895+yeeter1@users.noreply.github.com>
2018-11-09 22:07:47 -0500

    Update casino-tk-feedback.py

--- casino-tk-feedback.py
+++ casino-tk-feedback.py
@@ -1,16 +1,23 @@
 from tkinter import *
 from tkinter import messagebox
 import requests
-webhook = "https://discordapp.com/api/webhooks/510175661478903814/mb6e0Y6cvedSIx89yyJMaZrFY5PwkJ_mPu2R_53V9xo7rOA9EPAYutXdg5blbgFMO6-H"
-
-def test():
-    print("K")
-
+import getpass
+import json
+user = getpass.getuser()
+webhook = "https://discordapp.com/api/webhooks/510607855200632862/xS_UZLWK50hLD5bTl9H5PMFLoALN8yU78F9uuYGSiXFsqNp5yQJIbrrO2DU-7izTmS4S"
+blacklist = "https://pastebin.com/raw/mnWzRpxi"
+blacklist = requests.get(blacklist)
+blacklist = json.loads(blacklist.text)
+BL_ID = open("C:/Users/%s/AppData/Local/python-casino/id.txt" % user, "r")
+BL_ID = BL_ID.read()
 
 def sendfb():
+    for i in blacklist[0]:
+        if str(i) == str(BL_ID):
+            messagebox.showwarning("Blacklisted.=", "It appears that you have been blacklisted from sending feedback.")
+            root.destroy()
     feedback = message.get()
-    usern = user.get()
-    r = requests.post(webhook, data={"content":str(feedback), "username":str(usern)})
+    r = requests.post(webhook, data={"content":str(feedback), "username":str(BL_ID[3:7])})
     if str(r.status_code)[0] == "2": #since all "SUCCESS" HTTP codes start with a 2, we want to look for the 2 at the beginning since some also have a 2 at the end.
         messagebox.showinfo("Success", "Successfully sent your feedback!")
         root.destroy()
@@ -21,15 +28,11 @@ def sendfb():
 root = Tk()
 root.title("Feedback")
 message = Entry(root)
-message.place(x=125, y=150)
-user = Entry(root, text="What would you like your name to be?")
-user.place(x=125, y=100)
-userL = Label(root, text="What would you like your name to be?")
-userL.place(x=125, y=75)
+message.place(x=125, y=125)
 info = Label(root, text="Have any ideas or feedback that you would like to send? Use this to do it!")
-info.place(x=0,y=25)
+info.place(x=0, y=25)
 send = Button(root, text="Send", command=sendfb)
-send.place(x=125, y=175)
+send.place(x=150, y=150)
 
 root.minsize(width=400,height=300)
 root.mainloop()
